91c2abae3401a4f274b8cda9f8e0209ef6837ba8
Sean McGuire <sn475372@dal.ca>
2022-01-30 16:39:53 -0400

    first attempt at ET1

--- app/src/main/java/ca/dal/csci3130/quickcash/MainActivity.java
+++ app/src/main/java/ca/dal/csci3130/quickcash/MainActivity.java
@@ -1,10 +1,21 @@
 package ca.dal.csci3130.quickcash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import ca.dal.csci3130.quickcash.usermanagement.LoginActivity;
+import ca.dal.csci3130.quickcash.usermanagement.SignupActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button login;
+    private Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +23,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Add logic to handle the two buttons added in the UI
+
+        login = findViewById(R.id.loginButton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        signUp = findViewById(R.id.signUpButton);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
