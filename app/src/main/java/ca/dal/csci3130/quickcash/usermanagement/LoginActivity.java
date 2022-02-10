cadd69757198143362891d0bbcd7da0c0f6fa401
Adam Hawkins <ad373065@dal.ca>
2022-02-10 11:59:29 -0400

    ET2 & 3 for US3 &4 completed.  Login backend logic and validation methods finished

--- app/src/main/java/ca/dal/csci3130/quickcash/usermanagement/LoginActivity.java
+++ app/src/main/java/ca/dal/csci3130/quickcash/usermanagement/LoginActivity.java
@@ -9,8 +9,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.PatternsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.regex.Pattern;
 import ca.dal.csci3130.quickcash.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -31,14 +39,78 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        Button loginButtonCheck = (Button) findViewById(R.id.loginButtonCheckInfo);
 
-        String intentEmail = getIntent().getStringExtra("Email");
-        EditText email = (EditText) findViewById(R.id.etEmailId);
+        loginButtonCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] loginDetails = getLoginData();
+            }
+        });
+
+//        String intentEmail = getIntent().getStringExtra("Email");
+//        EditText email = (EditText) findViewById(R.id.etEmailId);
 //        if(!intentEmail.isEmpty()){
 //            email.append(intentEmail);
 //        }
 
+    }
 
+    /**
+     * Gets loginEmail and loginPassword attempted by the UI. Validates input.
+     * If the data is valid, returns a String[] array containing [0]loginEmail, [1]loinPassword
+     * else it will return null
+     *
+     * @return String[] loginDetails
+     */
+    private String[] getLoginData() {
+        String[] loginDetails = new String[2];
+        String loginEmail = ((EditText) findViewById(R.id.etEmailId)).getText().toString();
+        String loginPassword = ((EditText) findViewById(R.id.etPassword)).getText().toString();
+        if (!isEmpty(loginEmail, loginPassword)) {
+            if(isValidEmail(loginEmail)) {
+                return loginDetails;
+            }
+        }
+        return null;
+    }
 
+    /**
+     * Check if any login field is empty
+     *
+     * @param loginEmail
+     * @param loginPassword
+     * @return true or false
+     */
+    protected boolean isEmpty(String loginEmail, String loginPassword) {
+        boolean anyFieldsEmpty = loginEmail.isEmpty() || loginPassword.isEmpty();
+        if (anyFieldsEmpty) {
+            createToast(R.string.toast_missing_component);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * method to validate if the email entered is valid
+     *
+     * @param email
+     * @return true or false
+     */
+    protected boolean isValidEmail(String email) {
+        boolean isEmailValid = PatternsCompat.EMAIL_ADDRESS.matcher(email).matches();
+        if (!isEmailValid) {
+            createToast(R.string.toast_invalid_email);
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * method to create Toast message upon error
+     * @param messageId
+     */
+    protected void createToast(int messageId){
+        Toast.makeText(getApplicationContext(), getString(messageId), Toast.LENGTH_LONG).show();
     }
 }
