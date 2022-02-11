2982a04c0f41e44715858762780fb7732c94662c
Adam Hawkins <ad373065@dal.ca>
2022-02-11 15:27:24 -0400

    Added logic to prevent using Back button on Employer & Employee home pages, created Logout button on Employer & Employee home pages

--- app/src/main/java/ca/dal/csci3130/quickcash/home/EmployeeHomeActivity.java
+++ app/src/main/java/ca/dal/csci3130/quickcash/home/EmployeeHomeActivity.java
@@ -9,6 +9,11 @@ import ca.dal.csci3130.quickcash.R;
 public class EmployeeHomeActivity extends AppCompatActivity {
 
     @Override
+    //Prevent user from using back button once logged in
+    public void onBackPressed () {
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_home);
