21ae9fc93888c9316bdb7287913b61fd6cef93ed
mhamza71 <mh918605@dal.ca>
2022-03-12 23:31:01 -0400

    ET3, AT2

--- app/src/androidTest/java/ca/dal/csci3130/quickcash/jobmanagement/AvailableJobsActivityEspressoTest.java
+++ app/src/androidTest/java/ca/dal/csci3130/quickcash/jobmanagement/AvailableJobsActivityEspressoTest.java
@@ -1,9 +1,15 @@
 package ca.dal.csci3130.quickcash.jobmanagement;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.content.Intent;
 
@@ -20,6 +26,7 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import ca.dal.csci3130.quickcash.R;
+import ca.dal.csci3130.quickcash.ToastMatcher;
 import ca.dal.csci3130.quickcash.home.EmployeeHomeActivity;
 
 
@@ -36,6 +43,15 @@ public class AvailableJobsActivityEspressoTest {
     public void testMoveToAvailableJobsActivity() {
         onView(withId(R.id.btn_returnHome_employee)).perform(click());
         intended(hasComponent(EmployeeHomeActivity.class.getName()));
+    }
+
+    @Test
+    public void testNoJobsFound() {
+        onView(withId(R.id.editTextSearchJobType)).perform(clearText(), typeText("asdf"), closeSoftKeyboard());
+        onView(withId(R.id.editTextSearchPayRate)).perform(clearText(), typeText("900"), closeSoftKeyboard());
+        onView(withId(R.id.editTextSearchDuration)).perform(clearText(), typeText("1"), closeSoftKeyboard());
+        onView(withId(R.id.buttonApplyFilter)).perform(click());
+        onView(withText(R.string.no_job_found)).inRoot(new ToastMatcher()).check(matches(isDisplayed()));
     }
 
 
